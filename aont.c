1b800651909be2c9fea42a2bf2f8ac1365551f89
Austen Barker <atbarker@ucsc.edu>
2020-03-02 14:37:03 -0800

    commented out AES

--- aont.c
+++ aont.c
@@ -12,22 +12,14 @@
  *
  *Should operate of a 256 bit key to match the hash length
  */
-static int encrypt_payload(uint8_t *data, const size_t datasize, uint8_t *key, size_t keylength) {
+/*static int encrypt_payload(uint8_t *data, const size_t datasize, uint8_t *key, size_t keylength) {
     struct crypto_skcipher *tfm = NULL;
     struct skcipher_request *req = NULL;
     struct scatterlist sg;
     DECLARE_CRYPTO_WAIT(wait);
-    u8 iv[16];  /* AES-256-XTS takes a 16-byte IV */
-    //u8 key[64]; /* AES-256-XTS takes a 64-byte key */
+    u8 iv[16];  //AES-256-XTS takes a 16-byte IV
+    //u8 key[64]; //AES-256-XTS takes a 64-byte key
     int err;
-
-    /*
-     * Allocate a tfm (a transformation object) and set the key.
-     *
-     * In real-world use, a tfm and key are typically used for many
-     * encryption/decryption operations.  But in this example, we'll just do a
-     * single encryption operation with it (which is not very efficient).
-     */
 
     tfm = crypto_alloc_skcipher("xts(aes)", 0, 0);
     if (IS_ERR(tfm)) {
@@ -41,26 +33,17 @@ static int encrypt_payload(uint8_t *data, const size_t datasize, uint8_t *key, s
         goto out;
     }
 
-    /* Allocate a request object */
+    // Allocate a request object
     req = skcipher_request_alloc(tfm, GFP_KERNEL);
     if (!req) {
         err = -ENOMEM;
         goto out;
     }
 
-    /* Initialize the IV */
+    //Initialize the IV
     memset(iv, 0, 16);
     //get_random_bytes(iv, sizeof(iv));
 
-    /*
-     * Encrypt the data in-place.
-     *
-     * For simplicity, in this example we wait for the request to complete
-     * before proceeding, even if the underlying implementation is asynchronous.
-     *
-     * To decrypt instead of encrypt, just change crypto_skcipher_encrypt() to
-     * crypto_skcipher_decrypt().
-     */
     sg_init_one(&sg, data, datasize);
     skcipher_request_set_callback(req, CRYPTO_TFM_REQ_MAY_BACKLOG | CRYPTO_TFM_REQ_MAY_SLEEP, crypto_req_done, &wait);
     skcipher_request_set_crypt(req, &sg, &sg, datasize, iv);
@@ -76,7 +59,7 @@ out:
     skcipher_request_free(req);
     kfree(data);
     return err;
-}
+}*/
 
 static int decrypt_payload(uint8_t *data, const size_t datasize, uint8_t *key, size_t keylength){
     int err = 0;
