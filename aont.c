6d5792e9a0d89ffa04cec3c646b84719a982be77
Austen Barker <atbarker@ucsc.edu>
2020-03-02 14:48:52 -0800

    system written

--- aont.c
+++ aont.c
@@ -6,65 +6,125 @@
 #include <crypto/skcipher.h>
 
 
+struct tcrypt_result {
+    struct completion completion;
+    int err;
+};
+
+/* tie all data structures together */
+struct skcipher_def {
+    struct scatterlist sg;
+    struct crypto_skcipher *tfm;
+    struct skcipher_request *req;
+    struct tcrypt_result result;
+};
+
+/* Callback function */
+static void test_skcipher_cb(struct crypto_async_request *req, int error)
+{
+    struct tcrypt_result *result = req->data;
+
+    if (error == -EINPROGRESS)
+        return;
+    result->err = error;
+    complete(&result->completion);
+    pr_info("Encryption finished successfully\n");
+}
+
+/* Perform cipher operation */
+static unsigned int test_skcipher_encdec(struct skcipher_def *sk,
+                     int enc)
+{
+    int rc = 0;
+
+    if (enc)
+        rc = crypto_skcipher_encrypt(sk->req);
+    else
+        rc = crypto_skcipher_decrypt(sk->req);
+
+    switch (rc) {
+    case 0:
+        break;
+    case -EINPROGRESS:
+    case -EBUSY:
+        rc = wait_for_completion_interruptible(
+            &sk->result.completion);
+        if (!rc && !sk->result.err) {
+            reinit_completion(&sk->result.completion);
+            break;
+        }
+    default:
+        pr_info("skcipher encrypt returned with %d result %d\n",
+            rc, sk->result.err);
+        break;
+    }
+    init_completion(&sk->result.completion);
+
+    return rc;
+}
+
+
 //TODO, eliminate memcpy and do things in place
 /*
  *Because the Linux kernel interface is a place of nightmares
  *
  *Should operate of a 256 bit key to match the hash length
  */
-/*static int encrypt_payload(uint8_t *data, const size_t datasize, uint8_t *key, size_t keylength) {
-    struct crypto_skcipher *tfm = NULL;
+static int encrypt_payload(uint8_t *data, const size_t datasize, uint8_t *key, size_t keylength, int enc) {
+    struct skcipher_def sk;
+    struct crypto_skcipher *skcipher = NULL;
     struct skcipher_request *req = NULL;
-    struct scatterlist sg;
-    DECLARE_CRYPTO_WAIT(wait);
-    u8 iv[16];  //AES-256-XTS takes a 16-byte IV
-    //u8 key[64]; //AES-256-XTS takes a 64-byte key
-    int err;
+    uint8_t ivdata[16];
+    int ret = -EFAULT;
 
-    tfm = crypto_alloc_skcipher("xts(aes)", 0, 0);
-    if (IS_ERR(tfm)) {
-        pr_err("Error allocating xts(aes) handle: %ld\n", PTR_ERR(tfm));
-        return PTR_ERR(tfm);
+    skcipher = crypto_alloc_skcipher("cbc-aes-aesni", 0, 0);
+    if (IS_ERR(skcipher)) {
+        pr_info("could not allocate skcipher handle\n");
+        return PTR_ERR(skcipher);
     }
 
-    err = crypto_skcipher_setkey(tfm, key, sizeof(key));
-    if (err) {
-        pr_err("Error setting key: %d\n", err);
-        goto out;
-    }
-
-    // Allocate a request object
-    req = skcipher_request_alloc(tfm, GFP_KERNEL);
+    req = skcipher_request_alloc(skcipher, GFP_KERNEL);
     if (!req) {
-        err = -ENOMEM;
+        pr_info("could not allocate skcipher request\n");
+        ret = -ENOMEM;
         goto out;
     }
 
-    //Initialize the IV
-    memset(iv, 0, 16);
-    //get_random_bytes(iv, sizeof(iv));
+    skcipher_request_set_callback(req, CRYPTO_TFM_REQ_MAY_BACKLOG,
+                      test_skcipher_cb,
+                      &sk.result);
 
-    sg_init_one(&sg, data, datasize);
-    skcipher_request_set_callback(req, CRYPTO_TFM_REQ_MAY_BACKLOG | CRYPTO_TFM_REQ_MAY_SLEEP, crypto_req_done, &wait);
-    skcipher_request_set_crypt(req, &sg, &sg, datasize, iv);
-    err = crypto_wait_req(crypto_skcipher_encrypt(req), &wait);
-    if (err) {
-        pr_err("Error encrypting data: %d\n", err);
+    if (crypto_skcipher_setkey(skcipher, key, 32)) {
+        pr_info("key could not be set\n");
+        ret = -EAGAIN;
         goto out;
     }
 
-    pr_debug("Encryption was successful\n");
+    /* IV will be random */
+    //get_random_bytes(ivdata, 16);
+    memset(ivdata, 0, 16);
+
+    sk.tfm = skcipher;
+    sk.req = req;
+
+    /* We encrypt one block */
+    sg_init_one(&sk.sg, data, datasize);
+    skcipher_request_set_crypt(req, &sk.sg, &sk.sg, 16, ivdata);
+    init_completion(&sk.result.completion);
+
+    /* encrypt data */
+    ret = test_skcipher_encdec(&sk, enc);
+    if (ret)
+        goto out;
+
+    pr_info("Encryption triggered successfully\n");
+
 out:
-    crypto_free_skcipher(tfm);
-    skcipher_request_free(req);
-    kfree(data);
-    return err;
-}*/
-
-static int decrypt_payload(uint8_t *data, const size_t datasize, uint8_t *key, size_t keylength){
-    int err = 0;
-    
-    return err;
+    if (skcipher)
+        crypto_free_skcipher(skcipher);
+    if (req)
+        skcipher_request_free(req);
+    return ret;
 }
 
 //TODO change sizes here
@@ -89,7 +149,7 @@ int encode_aont_package(const uint8_t *data, size_t data_length, uint8_t **carri
     //generate key and IV
     //TODO figure out something else for the IV
     get_random_bytes(key, sizeof(key)); 
-    encrypt_payload(encode_buffer, cipher_size, key, 16);
+    encrypt_payload(encode_buffer, cipher_size, key, 16, 1);
 
     params.BlockBytes = rs_block_size;
     params.OriginalCount = data_blocks;
@@ -144,7 +204,7 @@ int decode_aont_package(uint8_t **carrier_blocks, uint8_t *data, size_t data_len
         key[i] = encode_buffer[cipher_size + i] ^ hash[i];
     }
 
-    decrypt_payload(encode_buffer, cipher_size, key, 16);
+    encrypt_payload(encode_buffer, cipher_size, key, 16, 0);
     if(memcmp(canary, &encode_buffer[data_length], 16)){
         return -1;
     }
