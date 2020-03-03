63e8ed271f57b2bfa9b41d0937b0f43e61391ff9
Austen Barker <atbarker@ucsc.edu>
2020-03-03 09:44:26 -0800

    revising

--- aont.c
+++ aont.c
@@ -6,60 +6,6 @@
 #include <crypto/skcipher.h>
 #include "aont.h"
 
-//block size in bytes, key size is assumed to be the same
-#define SPECK_BLOCK_SIZE 16
-
-#define ROR(x, r) ((x >> r) | (x << (64 - r)))
-#define ROL(x, r) ((x << r) | (x >> (64 - r)))
-#define R(x, y, k) (x = ROR(x, 8), x += y, x ^= k, y = ROL(y, 3), y ^= x)
-#define ROUNDS 32
-
-/**
- * pt: plaintext
- * ct: ciphertext
- * k: key
- * we assume that input arrays are of length 2 so we get 128 bits
- * Should generate the key on the fly, just for simplicity sake
- * Better performance can be had by computing round keys once.
- * This function is obtained from the following paper, https://eprint.iacr.org/2013/404
- */
-static void speck_encrypt(uint64_t ct[2], uint64_t const pt[2], uint64_t const K[2])
-{
-    uint64_t y = pt[0], x = pt[1], b = K[0], a = K[1];
-    int i;
-
-    R(x, y, b);
-    for (i = 0; i < ROUNDS - 1; i++) {
-        R(a, b, i);
-        R(x, y, b);
-    }
-
-    ct[0] = y;
-    ct[1] = x;
-}
-
-static void speck_128_hash(uint8_t *data, size_t data_length, uint8_t* hash){
-    uint64_t seed[2] = {0,0};
-    uint32_t rounds = data_length/SPECK_BLOCK_SIZE;
-    uint64_t i, j, ctr[2], temp[2];
-
-    j = 0;
-    ctr[0] = 0;
-    ctr[1] = 0;
-    memset(hash, 0, 16);
-
-    for(i = 0; i < rounds; i++){
-	temp[0] = ((uint64_t *)data)[j + 0];
-        temp[1] = ((uint64_t *)data)[j + 1];
-        speck_encrypt(temp, ctr, seed);
-        ((uint64_t*)hash)[0] ^= temp[0];
-        ((uint64_t*)hash)[1] ^= temp[1];
-        ctr[0]++;
-        if(ctr[0] == 0) ctr[1]++;
-	j += 2;
-    }
-}
-
 
 
 /* Callback function */
@@ -196,7 +142,8 @@ int encode_aont_package(const uint8_t *data, size_t data_length, uint8_t **carri
     params.OriginalCount = data_blocks;
     params.RecoveryCount = parity_blocks;
 
-    speck_128_hash(encode_buffer, cipher_size, hash);    
+    //speck_128_hash(encode_buffer, cipher_size, hash);
+    memset(hash, 0, 16);    
 
     for (i = 0; i < 16; i++) {
         encode_buffer[cipher_size + i] = key[i] ^ hash[i];
@@ -236,7 +183,8 @@ int decode_aont_package(uint8_t **carrier_blocks, uint8_t *data, size_t data_len
         memcpy(&encode_buffer[rs_block_size * i], carrier_blocks[i], rs_block_size);
     }
 
-    speck_128_hash(encode_buffer, cipher_size, hash);
+    //speck_128_hash(encode_buffer, cipher_size, hash);
+    memset(hash, 0, 16);
 
     for(i = 0; i < 16; i++){
         key[i] = encode_buffer[cipher_size + i] ^ hash[i];
