585fc80884bf354541e4e09b7b4bb79c67c93a7e
austen barker <atbarker@ucsc.edu>
2020-03-19 11:13:26 -0700

    committing the cardinal sin of writing to userspace files from the kernel

--- main.c
+++ main.c
@@ -10,31 +10,81 @@
 #include <linux/types.h>
 #include "cauchy_rs.h"
 #include "aont.h"
+#include "read_file.h"
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("AUSTEN BARKER");
 
+//Just do 4MB
+#define FILE_SIZE 4194303
+
+char* input_file[] = {"/home/austen/Documents/course-cs111-s19.pdf"};
+char* encrypt_input_file[] = {"/home/austen/Documents/io-cs111-s19.pdf"};
+char* output_file[] = {"/home/austen/Documents/course-cs111-s19-encoded.txt"};
+char* encrypt_output_file[] = {"/home/austen/Documents/io-cs111-s19-encrypted.txt"};
+
+static int read_file(uint8_t **data, size_t data_blocks, char* path){
+    struct file* file = NULL;
+    int ret = 0;
+    int i;
+    loff_t offset = 0;
+    
+    file = file_open(path, O_RDONLY, 0);
+    for(i = 0; i < data_blocks; i++){
+	offset = i * 4096;
+        ret = kernel_read(file, data[i], 4096, &offset);
+    }
+    if (ret < 0){
+        printk(KERN_INFO "Kernel Read Failed: %d\n", ret);
+    }
+    file_close(file);
+    return ret;
+}
+
+static int write_file(uint8_t **data, size_t data_blocks, size_t share_size, char* path){
+    struct file* file = NULL;
+    int ret = 0;
+    int i;
+    loff_t offset = 0;
+
+    file = file_open(path, O_CREAT|O_WRONLY|O_TRUNC, 0);
+    for(i = 0; i < data_blocks; i++){
+	offset = i * 4096;
+        ret = kernel_write(file, data[i], 4096, &offset);
+    }
+    if (ret < 0){
+        printk(KERN_INFO "Kernel Read Failed: %d\n", ret);
+    }
+    file_close(file);
+    return ret;
+}
+
 static int test_aont(void){
     uint8_t *data = kmalloc(4096, GFP_KERNEL);
-    size_t data_blocks = 2;
-    size_t parity_blocks = 3;
+    size_t data_blocks = 1024;
+    size_t parity_blocks = 1024;
     size_t data_length = 4096;
     uint8_t **shares = kmalloc(sizeof(uint8_t*) * (data_blocks + parity_blocks), GFP_KERNEL);
     int i = 0;
     struct timespec timespec1, timespec2;
     uint8_t erasures[0] = {};
     uint8_t num_erasures = 0;
+    size_t share_size = get_share_size(data_length, data_blocks);
 
-    get_random_bytes(data, 4096);
+    //get_random_bytes(data, 4096);
 
     //For this example each share is the size of the original AONT payload
-    for(i = 0; i < data_blocks + parity_blocks; i++) shares[i] = kmalloc(get_share_size(data_length, data_blocks), GFP_KERNEL);
+    for(i = 0; i < data_blocks + parity_blocks; i++) shares[i] = kmalloc(share_size, GFP_KERNEL);
+
+    read_file(shares, data_blocks, input_file[0]);    
 
     getnstimeofday(&timespec1); 
     encode_aont_package(data, data_length, shares, data_blocks, parity_blocks);
     getnstimeofday(&timespec2);
     printk(KERN_INFO "Encode took: %ld nanoseconds",
 (timespec2.tv_sec - timespec1.tv_sec) * 1000000000 + (timespec2.tv_nsec - timespec1.tv_nsec));
+
+    write_file(shares, data_blocks, share_size, output_file[0]);
 
     getnstimeofday(&timespec1);
     decode_aont_package(data, data_length, shares, data_blocks, parity_blocks, erasures, num_erasures);
--- read_file.h
+++ read_file.h
@@ -1,62 +1,11 @@
-#ifndef ENTROPY_H
-#define ENTROPY_H
+#ifndef READ_FILE_H
+#define READ_FILE_H
 
 #include <linux/slab.h>
+#include <linux/fs.h>
 
-//Hash table of size 2^16 with name dm_afs_ht
-//hopefully don't need anything bigger than that
-#define HASH_TABLE_ORDER 16
-#define HASH_TABLE_NAME dm_afs_ht
+struct file* file_open(char* path, int flags, int rights);
 
-/**
- * A single entry in the hash table
- * @key, the hashed filename
- * @filename, name of the specified file
- * @file_size, size of the specified file (used for allocation)
- * @hash_list, list of hash nodes needed for the hash table to work
- */
-struct entropy_hash_entry{
-    uint64_t key;
-    char* filename;
-    size_t file_size;
-    struct hlist_node hash_list;
-};
-
-/**
- * Global entropy context for AFS
- * @number_of_files, number of files in the directory (non-recursive search, disregards directories)
- * @directory_name, user specified name of the entropy directory
- * @directory_name_length, used for bounds checking on strings
- * @file_list, list of file names in the directory
- */
-struct entropy_context{
-    uint32_t number_of_files;
-    char* directory_name;
-    size_t directory_name_length;
-    char** file_list;
-};
-
-/**
- * Hash table constructor
- * Call in dm target constructor with entropy directory name as input
- */
-void build_entropy_ht(char* directory_name, size_t name_length);
-
-/**
- * Call in dm target destructor
- */
-void cleanup_entropy_ht(void);
-
-/**
- * Allocate an entropy block (if new blocks are needed)
- * Populates the variables for the filename hash, block pointer, and data block
- */
-void allocate_entropy(uint64_t *filename_hash, uint32_t *block_pointer, uint8_t *entropy_block);
-
-/**
- * Reads a specified entropy block from the disk
- */
-int read_entropy(uint64_t filename_hash, uint32_t block_pointer, uint8_t* block);
-
+void file_close(struct file* file);
 
 #endif
